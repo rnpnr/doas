c1479b5678b632e5545fe2b97b66fa1dab2ae245
0x766F6964 <palamar@ualberta.ca>
2019-12-08 12:55:35 -0700

    add files needed for compatibility
    *.c files taken directly from OpenBSD

---
+++ compat/stdlib.h
@@ -0,0 +1,6 @@
+#include_next <stdlib.h>
+#include <stdint.h>
+void *reallocarray(void *, size_t, size_t);
+long long strtonum(const char *, long long, long long, const char **);
+void setprogname(const char *);
+extern char *__progname;
